c5727dbd5baa5fc3c2b6ea77ec9dc5915fef615b
ivy <ivyhzyb@gmail.com>
2023-09-21 21:12:04 +0800

    Add Audio

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ import RPi.GPIO as GPIO
 import time
 import picamera
 
+AMP_CONTROL_PIN = 3
+PIR_PIN = 7
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.IN)  # Read output from PIR motion sensor
+GPIO.setup(PIR_PIN, GPIO.IN)  # Read output from PIR motion sensor
 camera = picamera.PiCamera()
-# GPIO.setup(3, GPIO.OUT)         #Audio output pin
+GPIO.setup(AMP_CONTROL_PIN, GPIO.OUT)         #Audio output pin
 
 while True:
-    i = GPIO.input(7)
+    i = GPIO.input(PIR_PIN)
     if i == 0:  # When output from motion sensor is LOW
         print("No intruders", i)
-        #		GPIO.output(3, 0)  #Turn OFF Audio
+        GPIO.output(AMP_CONTROL_PIN, GPIO.LOW)  #Turn OFF Audio
         time.sleep(0.1)
     elif i == 1:  # When output from motion sensor is HIGH
         print("Intruder detected", i)
-        #		GPIO.output(3, 1)  #Turn ON Audio
+        GPIO.output(AMP_CONTROL_PIN, GPIO.HIGH)  #Turn ON Audio
         camera.capture("intruder.jpg")
         time.sleep(0.1)
